7e390366fff15681caabaced76b87d914205a3f6
Luis Majano <lmajano@gmail.com>
2024-04-23 06:02:39 +0200

    directory start

--- src/main/java/ortus/boxlang/modules/bxesapi/util/KeyDirectory.java
+++ src/main/java/ortus/boxlang/modules/bxesapi/util/KeyDirectory.java
@@ -1,17 +1,49 @@
+/**
+ * [BoxLang]
+ *
+ * Copyright [2023] [Ortus Solutions, Corp]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
 package ortus.boxlang.modules.bxesapi.util;
 
 import ortus.boxlang.runtime.scopes.Key;
 
+/**
+ * A directory of keys used in the bx-esapi module.
+ */
 public class KeyDirectory {
 
-	public static final Key	css				= Key.of( "css" );
-	public static final Key	dn				= Key.of( "dn" );
-	public static final Key	html			= Key.of( "html" );
-	public static final Key	htmlAttribute	= Key.of( "htmlAttribute" );
-	public static final Key	javascript		= Key.of( "javascript" );
-	public static final Key	ldap			= Key.of( "ldap" );
-	public static final Key	url				= Key.of( "url" );
-	public static final Key	xml				= Key.of( "xml" );
-	public static final Key	xmlAttribute	= Key.of( "xmlAttribute" );
-	public static final Key	xPath			= Key.of( "xPath" );
+	public static final Key	css						= Key.of( "css" );
+	public static final Key	db2						= Key.of( "db2" );
+	public static final Key	dialect					= Key.of( "dialect" );
+	public static final Key	dn						= Key.of( "dn" );
+	public static final Key	encodeForCSS			= Key.of( "encodeForCSS" );
+	public static final Key	encodeForDN				= Key.of( "encodeForDN" );
+	public static final Key	encodeForHTML			= Key.of( "encodeForHTML" );
+	public static final Key	encodeForHTMLAttribute	= Key.of( "encodeForHTMLAttribute" );
+	public static final Key	encodeForJavaScript		= Key.of( "encodeForJavaScript" );
+	public static final Key	encodeForLDAP			= Key.of( "encodeForLDAP" );
+	public static final Key	encodeForURL			= Key.of( "encodeForURL" );
+	public static final Key	encodeForXML			= Key.of( "encodeForXML" );
+	public static final Key	encodeForXMLAttribute	= Key.of( "encodeForXMLAttribute" );
+	public static final Key	encodeForXPath			= Key.of( "encodeForXPath" );
+	public static final Key	html					= Key.of( "html" );
+	public static final Key	htmlAttribute			= Key.of( "htmlAttribute" );
+	public static final Key	javascript				= Key.of( "javascript" );
+	public static final Key	ldap					= Key.of( "ldap" );
+	public static final Key	mysql					= Key.of( "mysql" );
+	public static final Key	mysql_ansi				= Key.of( "mysql_ansi" );
+	public static final Key	oracle					= Key.of( "oracle" );
+	public static final Key	url						= Key.of( "url" );
+	public static final Key	xml						= Key.of( "xml" );
+	public static final Key	xmlAttribute			= Key.of( "xmlAttribute" );
+	public static final Key	xPath					= Key.of( "xPath" );
 }
