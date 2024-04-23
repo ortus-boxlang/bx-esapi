a1e481e0a2934bed96dc7e14001e2bcc73e8a05c
Luis Majano <lmajano@gmail.com>
2024-04-23 06:01:42 +0200

    added a quick key directory

---
+++ src/main/java/ortus/boxlang/modules/bxesapi/util/KeyDirectory.java
@@ -0,0 +1,17 @@
+package ortus.boxlang.modules.bxesapi.util;
+
+import ortus.boxlang.runtime.scopes.Key;
+
+public class KeyDirectory {
+
+	public static final Key	css				= Key.of( "css" );
+	public static final Key	dn				= Key.of( "dn" );
+	public static final Key	html			= Key.of( "html" );
+	public static final Key	htmlAttribute	= Key.of( "htmlAttribute" );
+	public static final Key	javascript		= Key.of( "javascript" );
+	public static final Key	ldap			= Key.of( "ldap" );
+	public static final Key	url				= Key.of( "url" );
+	public static final Key	xml				= Key.of( "xml" );
+	public static final Key	xmlAttribute	= Key.of( "xmlAttribute" );
+	public static final Key	xPath			= Key.of( "xPath" );
+}
